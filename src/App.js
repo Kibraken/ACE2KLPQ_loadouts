db76a20274917624e15b3b041ed612e5d25591d1
Kibra <kobrakum@gmail.com>
2019-09-16 01:19:56 +0300

    small changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import React from 'react';
 //   "CFP_OPS2017_Helmet_White","CFP_Face_Wear_Spook",["Binocular","","","",[],[],""],["ItemMap","ItemGPS","ItemRadio","ItemCompass","ItemWatch","CUP_NVG_PVS15_winter"]
 // ];
 
-let data = [["CUP_arifle_AK101_GL","RH_qdss_nt4","rhs_acc_2dpZenit_ris","",["CUP_30Rnd_556x45_AK",30],["CUP_1Rnd_HE_GP25_M",1],""],[],["CUP_hgun_Glock17_blk","muzzle_snds_L","CUP_acc_Glock17_Flashlight","",["CUP_17Rnd_9x19_glock17",17],[],""],["CFP_U_Crye_M90",[["FirstAidKit",1],["Chemlight_green",1,1],["CUP_17Rnd_9x19_glock17",1,17]]],["CFP_FAPC_Operator_OGA_OD",[["MiniGrenade",2,1],["SmokeShell",1,1],["SmokeShellGreen",1,1],["Chemlight_green",1,1],["CUP_17Rnd_9x19_glock17",2,17]]],["CFP_Kitbag_White",[["MineDetector",1],["DemoCharge_Remote_Mag",5,1],["SatchelCharge_Remote_Mag",1,1],["APERSTripMine_Wire_Mag",1,1],["APERSMine_Range_Mag",1,1],["CUP_1Rnd_HE_GP25_M",4,1]]],"CFP_OPS2017_Helmet_White","CFP_Face_Wear_Spook",["Binocular","","","",[],[],""],["ItemMap","ItemGPS","ItemRadio","ItemCompass","ItemWatch","CUP_NVG_PVS15_winter"]];
+let data = [["CUP_arifle_AK101_GL","RH_qdss_nt4","rhs_acc_2dpZenit_ris","",["CUP_30Rnd_556x45_AK",30],["CUP_1Rnd_HE_GP25_M",1],""],[],["CUP_hgun_Glock17_blk","muzzle_snds_L","CUP_acc_Glock17_Flashlight","",["CUP_17Rnd_9x19_glock17",17],[],""],["CFP_U_Crye_M90",[["FirstAidKit",1],["Chemlight_green",1,1],["CUP_17Rnd_9x19_glock17",1,17]]],["CFP_FAPC_Operator_OGA_OD",[["MiniGrenade",2,1],["SmokeShell",1,1],["SmokeShellGreen",1,1],["Chemlight_green",1,1],["CUP_17Rnd_9x19_glock17",2,17]]],["CFP_Kitbag_White",[["MineDetector",1],["DemoCharge_Remote_Mag",5,1],["SatchelCharge_Remote_Mag",1,1],["APERSTripMine_Wire_Mag",1,1],["APERSMine_Range_Mag",1,1],["CUP_1Rnd_HE_GP25_M",4,1]]],"CFP_OPS2017_Helmet_White","CFP_Face_Wear_Spook",["Binocular","","","",[],[],""],["ItemMap","ItemGPS","ItemRadio","ItemCompass","ItemWatch","CUP_NVG_PVS15_winter"]]
 
 //[[],[],[],[],[],[],"","",[],["","","","","",""]]
 
@@ -69,40 +69,20 @@ class App extends React.Component {
     })
     return newArr
   }
-  parseUniformFn=(exportData)=>{
+  parseUniformFn=(exportData)=>{    
     let uniform = []
     exportData.forEach((i,index)=>{      
       switch(index){
         case 3:
-          if(i.length !== 0){
-            return uniform = [...uniform, i[0]]
-          }else{
-            return;
-          }
-        case 4: 
-          if(i.length !== 0){
-            return uniform = [...uniform, i[0]]
-          }else{
-            return;
-          }
+          return i[0].length !== 0 && typeof(i[0]) === "string" && (uniform = [...uniform, i[0]]);
+        case 4:         
+          return i[0].length !== 0 && typeof(i[0]) === "string" && (uniform = [...uniform, i[0]]);          
         case 5:  
-          if(i.length !== 0){
-            return uniform = [...uniform, i[0]]
-          }else{
-            return;
-          }
+          return i[0].length !== 0 && typeof(i[0]) === "string" && (uniform = [...uniform, i[0]]);
         case 6:  
-          if(i.length !== 0){            
-            return uniform = [...uniform, i]
-          }else{
-            return;
-          }
+          return i.length !== 0 && typeof(i) === "string" && (uniform = [...uniform, i]);          
         case 7:  
-          if(i.length !== 0){
-            return uniform = [...uniform, i]
-          }else{
-            return;
-          }
+          return i.length !== 0 && typeof(i) === "string" && (uniform = [...uniform, i]);
         default:
           break;
       }      
