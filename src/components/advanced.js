cabd94557fe5bbfc128d003b5d96c9d02c500ff5
Kibra <kobrakum@gmail.com>
2019-10-01 10:13:55 +0300

    bug fixes

--- src/components/advanced.js
+++ src/components/advanced.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { roles, additionaloptionsTemplate, classTemplate } from '../misc/data';
+import { roles, additionaloptionsTemplate } from '../misc/data';
 import { ParseLoadout } from '../misc/parsConverter';
 
 export const AdvancedMainWrap = styled.div`
@@ -167,7 +167,8 @@ class Advanced extends React.Component {
 		importArr: false,
 		currentSelection: 'squadLeader',
 		classes: roles,
-		additionaloptions: additionaloptionsTemplate
+		additionaloptions: additionaloptionsTemplate,
+		saveImport: false
 	};
 	//TODO: checkbox "keep this export after convertion"
 	handleCheckboxChange = id => {
@@ -175,25 +176,20 @@ class Advanced extends React.Component {
 		let { currentSelection } = this.state;
 		classes[currentSelection].classOptions[id] = !classes[currentSelection]
 			.classOptions[id];
-
 		this.setState({ classes });
 	};
 	handleCheckboxChangeClass = event => {
 		let classes = { ...this.state.classes };
-		let id = event.currentTarget.id.split('_')[0];
-		console.log(id);
-		console.log(event.currentTarget.id);
+		//let id = event.currentTarget.id.split('_')[0];
+		let id = event.currentTarget.id;
 		classes[id].classOptions.isChecked = !classes[id].classOptions.isChecked;
-
 		this.setState({ classes });
 	};
-	handleClassSelection = id => {
-		console.log(id);
-		console.log('selected', this.state.currentSelection);
+	handleClassSelection = (id, index) => {
 		let classes = { ...this.state.classes };
-		classes[id].classOptions.isSelected = !classes[id].classOptions.isSelected;
+		//classes[id].classOptions.isSelected = !classes[id].classOptions.isSelected;
+		classes[id].classOptions.isSelected = true;
 		classes[this.state.currentSelection].classOptions.isSelected = false;
-		//squadLeader
 		this.setState({ currentSelection: id, classes });
 	};
 	handleNumInput = (event, i, type) => {
@@ -210,7 +206,12 @@ class Advanced extends React.Component {
 	};
 	advConvert = async () => {
 		let classes = { ...this.state.classes };
-		let { additionaloptions, importArr, currentSelection } = this.state;
+		let {
+			additionaloptions,
+			importArr,
+			currentSelection,
+			saveImport
+		} = this.state;
 		if (importArr === false) {
 			return console.log('ebanulsya?');
 		}
@@ -222,49 +223,105 @@ class Advanced extends React.Component {
 		const convert = await new ParseLoadout(importArr, ammo);
 		classes[currentSelection].classLoadout = await convert.convertFn();
 		classes[currentSelection].classOptions.converted = true;
-
-		this.setState({ classes });
-		console.log('test', this.state.classes);
+		if (saveImport === true) {
+			this.setState({ classes });
+		} else {
+			this.setState({ classes, importArr: false });
+		}
 	};
-	handleText = event => {
-		let txt = JSON.parse(event.target.value);
-		this.setState({ importArr: txt });
+	handleText = (event, type) => {
+		if (type === 'exp') {
+			let txt = JSON.parse(event.target.value);
+			this.setState({ importArr: txt });
+		}
+		if (type === 'class') {
+			//todo check for unique name
+			let txt = event.target.value;
+			let { currentSelection } = this.state;
+			let classes = { ...this.state.classes };
+			classes[currentSelection].className = txt.length === 0 ? '_' : txt;
+			this.setState({ classes });
+		}
+		// if (type === 'class+') {
+		// 	let txt = JSON.parse(event.target.value);
+		// 	let { currentSelection } = this.state;
+		// 	let classes = { ...this.state.classes };
+
+		// тупо, по букве в массив наверное будет засовывать, перепилить нужно
+		// 	classes[currentSelection].classTags = [
+		// 		...classes[currentSelection].classTags,
+		// 		txt
+		// 	];
+		// 	this.setState({ classes });
+		// }
 	};
 	addNewRole = () => {
-		//classTemplate
 		let classes = { ...this.state.classes };
 		let num = Object.keys(classes).length - 11;
-		classes[`CustomRole${num}`] = { ...classTemplate };
-		classes[`CustomRole${num}`].className = `CustomClass${num}`;
-
+		classes[`CustomRole${num}`] = {
+			className: '',
+			classTags: [],
+			classLoadout: {},
+			classOptions: {
+				binocular: false,
+				gps: true,
+				radioSq: true,
+				radioFt: true,
+				isSelected: false,
+				isChecked: true,
+				mgMags: false,
+				converted: false,
+				nvg: false,
+				map: false,
+				compass: false
+			}
+		};
+		classes[`CustomRole${num}`].className = `CustomRole${num}`;
 		this.setState({ classes });
 	};
+	saveThisimport = () => this.setState({ saveImport: !this.state.saveImport });
+	exportToFile = () => {};
+
 	render() {
 		const {
 			classes,
 			additionaloptions,
 			currentSelection,
-			importArr
+			importArr,
+			saveImport
 		} = this.state;
+
 		return (
 			<AdvancedMainWrap>
 				<InputWrap>
 					<LoadInput
 						placeholder={'place ACE export here'}
-						onChange={e => this.handleText(e)}
-						value={importArr ? JSON.stringify(importArr) : null}
+						onChange={e => this.handleText(e, 'exp')}
+						value={importArr ? JSON.stringify(importArr) : undefined}
 					>
 						{/* {importArr !== false && JSON.stringify(importArr)} */}
 					</LoadInput>
+					<label>
+						<Checkbox checked={saveImport} onChange={() => this.saveThisimport()} />
+						<span>{`Save this input after convertion`}</span>
+					</label>
 					<Console>Loadout tips, warnings</Console>
 					<ClassInfoWrap>
 						<ClassInfoHeader>Selected class information</ClassInfoHeader>
 						<TextHeader>Class Name</TextHeader>
 						<ClassNameInput
-							value={classes[currentSelection].className}
+							value={
+								classes[currentSelection].className
+									? classes[currentSelection].className
+									: undefined
+							}
+							onChange={e => this.handleText(e, 'class')}
 						></ClassNameInput>
 						<TextHeader>Class Tags</TextHeader>
-						<ClassTags value={classes[currentSelection].classTags}></ClassTags>
+						<ClassTags
+							value={classes[currentSelection].classTags}
+							onChange={e => this.handleText(e, 'class+')}
+						></ClassTags>
 					</ClassInfoWrap>
 					<ClassCheckboxesWrap>
 						<TextHeader>Additional class options:</TextHeader>
@@ -306,21 +363,22 @@ class Advanced extends React.Component {
 				<ClassManagmentWrap>
 					<SaveToClass onClick={() => this.advConvert()}>SaveToClass</SaveToClass>
 					<ClassList>
-						{Object.keys(classes).map(i => {
+						{Object.keys(classes).map((i, index) => {
 							return (
 								<ClassWrap
 									key={i}
 									selected={classes[i].classOptions.isSelected}
 									checked={classes[i].classOptions.isChecked}
 									converted={classes[i].classOptions.converted}
-									onClick={() => this.handleClassSelection(i)}
+									onClick={() => this.handleClassSelection(i, index)}
 								>
 									<label>
 										<Checkbox
 											checked={classes[i].classOptions.isChecked}
 											//checked={this.state.classes[i].isSelected}
 											onChange={e => this.handleCheckboxChangeClass(e)}
-											id={`${i}_checkbox`}
+											//id={`${i}_checkbox`}
+											id={i}
 										/>
 									</label>
 									<Class>{classes[i].className}</Class>
